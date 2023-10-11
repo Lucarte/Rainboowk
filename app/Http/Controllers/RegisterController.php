30228294dc5457cfcf91eddcd09e77d8519b5669
LucArte <96082778+Lucarte@users.noreply.github.com>
2023-10-11 20:34:17 +0200

    First Commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password'))
+        ]);
+
+        Auth::login($user);
+        $request->session()->regenerate();
+
+        return response()->json(status: 201);
+    }
+}
